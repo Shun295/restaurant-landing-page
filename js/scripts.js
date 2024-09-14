1197510130c8f80b87d18e7b0c368bde324a1bad
Shun295 <166832814+Shun295@users.noreply.github.com>
2024-09-14 16:52:51 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+//jquery script
+//scripts for the testimonial carousel
+$('document').ready(function () {
+    $("#subscribe").click(function (event) {
+        event.preventDefault();
+        var subscribedemail = $("#emailaddress").val();
+        alert(subscribedemail + " has been successfully subscribed. Thank you for joining our mailing list. ");
+        $("#emailaddress").val(""); //clear input field after successfully subscribing.
+
+    });
+
+});
